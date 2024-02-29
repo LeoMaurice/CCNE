7e2edd789a53e81c446c92dadaaf4066ee38b7d6
LeoMaurice <leopold.maurice@m4x.org>
2024-02-29 17:19:38 +0100

    descrptive fig apart complexheatmap : ok

--- src/helpers/lexico_helper.R
+++ src/helpers/lexico_helper.R
@@ -1,6 +1,6 @@
 has_words <- function(sentence, words) {
   # Check if any of the words are in the sentence
-  test <- sapply(words, function(word) grepl(paste0("\\b", word, "\\b"), sentence))
+  test <- sapply(words, function(word) grepl(paste0("\\b", word, "\\b"), sentence, ignore.case = TRUE))
   # Return TRUE if any word is found, FALSE otherwise
   return(any(test))
 }
