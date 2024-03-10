3719932e7e05874e35f0414ae4d7bc8fed198f3a
LeoMaurice <leopold.maurice@m4x.org>
2024-03-11 00:25:55 +0100

    theme_ready : ok t_stat_pvalue : not working. How to get mapping ?

---
+++ src/helpers/figures.R
@@ -0,0 +1,62 @@
+require(pacman,quietly = T)
+pacman::p_load(rstatix,
+               
+               ggplot2,
+               gridExtra,
+               ggprism,
+               ggpubr,
+               ggcorrplot)
+
+theme_ready <-
+  function(text_size = 14,
+           default_theme = theme_pubclean()) {
+    return(
+      default_theme +
+        theme(
+          text = element_text(size = text_size),
+          axis.title = element_text(size = text_size + 2, face =
+                                      "bold"),
+          axis.text = element_text(size = text_size),
+          legend.title = element_text(size = text_size),
+          legend.text = element_text(size = text_size - 2)
+        )
+    )
+  }
+
+t_test_pvalue <-
+  function(
+    mapping = NULL,
+    data = NULL,
+    group.by_var,
+    p.adjust.method = "BH",
+    cutpoints = c(0, 1e-04, 0.001, 0.01, 0.05, 0.1, 1),
+    symbols = c("****", "***", "**", "*", "•", "ns"),
+    significance = c("p < 0.0001", "p < 0.001", "p < 0.01", 
+                    "p < 0.05", "p < 0.1", "p>0.1"),
+    inherit.aes = TRUE
+    ) {
+
+    y_var <- deparse(mapping$y)
+    x_var <- deparse(mapping$x)
+    
+    signif_caption_text <- paste("Significance levels:\n",
+                                paste(symbols, ":", significance, collapse = ", "))
+    
+    test_result.t <- data |>
+    group_by(.add = group.by_var) |>
+    t_test(as.formula(paste(y_var, "~", x_var)), 
+           p.adjust.method = p.adjust.method) |> 
+    add_significance(cutpoints = cutpoints,
+                     symbols = symbols) |>
+    add_xy_position()
+  
+  return(
+    stat_pvalue_manual(
+      test_result.t,
+      label = "p.adj.signif",
+      hide.ns = T,
+      step.group.by = group.by_var
+    )+
+      labs(subtitle = signif_caption_text)
+  )
+}
