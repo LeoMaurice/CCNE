4f48d3d34f120f9a38594623683a894a19897dd9
LeoMaurice <leopold.maurice@m4x.org>
2024-05-23 12:20:24 +0200

    descriptive members

--- src/helpers/database_creation.R
+++ src/helpers/database_creation.R
@@ -26,6 +26,28 @@ president_CCNE_by_date <- Vectorize(function(date){
     return(facteur_presidents[1]) #Jean Bernard
 })
 
+president_CCNE_by_year <- Vectorize(function(year){
+  # Définir l'ordre des niveaux du facteur
+  ordre_presidents <- c("Bernard (83-91)", "Changeux (92-99)", "Sicard (00-08)", 
+                        "Grimfeld (09-11)", "Ameisen (12-15)", "Delfraissy (16-)")
+  
+  # Définir le facteur avec les niveaux dans l'ordre spécifié
+  facteur_presidents <- factor(ordre_presidents, levels = ordre_presidents)
+  
+  if(year>=2016)
+    return(facteur_presidents[6]) #Jean-François Delfraissy
+  else if(year >= 2012)
+    return(facteur_presidents[5]) #Jean Claude Ameisen
+  else if(year >= 2008)
+    return(facteur_presidents[4]) #Alain Grimfeld
+  else if(year >= 1999)
+    return(facteur_presidents[3]) #Didier Sicard
+  else if(year >= 1992)
+    return(facteur_presidents[2]) #Jean-Pierre Changeux
+  else
+    return(facteur_presidents[1]) #Jean Bernard
+})
+
 open_avis <- function(rescrap_texte=FALSE){
   if(rescrap_texte){
     # Définir le chemin vers le dossier contenant les fichiers PDF
