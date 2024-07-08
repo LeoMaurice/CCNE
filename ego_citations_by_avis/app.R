4e722ce67f661cc501e37a0edec95efa1864eee4
LeoMaurice <leopold.maurice@m4x.org>
2024-07-08 21:01:18 +0200

    ego citation parfait

--- ego_citations_by_avis/app.R
+++ ego_citations_by_avis/app.R
@@ -1,123 +1,11 @@
-# Charger les bibliothèques nécessaires
-require(pacman,quietly = T)
-
-pacman::p_load(shiny,
-               tidyverse,
-               igraph,
-               networkD3,
-               visNetwork,
-               tidygraph,
-               leaflet)
-
-source("../src/helpers/database_creation.R")
-source("../src/helpers/figures.R")
-
-# Ouverture données
-
-designation_recoding <- Vectorize(function(type, Auteur_Institution, Institution_Domaine, freq_auteur, freq) {
-  if (type == "classic" && freq_auteur >= 2) {
-    return(Auteur_Institution)
-  } else if (type == "collectif") {
-    return("Collectif")
-  } else if (type == "presse spécialisée" && freq < 2) {
-    return("Presse Spécialisée diverse")
-  } else if (type == "presse" && freq < 2) {
-    return("Presse diverse")
-  } else if (type == "loi" && freq < 2) {
-    return("Loi diverse")
-  } else if (type == "comité d'éthique" && freq < 2) {
-    return("Comité d'éthique divers")
-  } else if (type == "autre pays" && freq < 2) {
-    return("Comité d'éthique divers")
-  } else {
-    return(Institution_Domaine)
-  }
-})
-
-
-metadata_avis <- open_metadata()
-
-metadata_citation <- read_excel("../data/raw/collected_metadata/metadata_citation.xlsx", 
-                                col_types = c("text", "text", "skip", 
-                                              "text", "text", "text", "skip", "skip", 
-                                              "skip"))%>%
-  separate_rows(cité_dans_avis_num, sep = ";")%>%
-  separate_rows(Institution_Domaine, sep = ";")%>%
-  group_by(Auteur_Institution)%>%
-  mutate(freq_auteur = n())%>%
-  ungroup()%>%
-  group_by(Institution_Domaine)%>%
-  mutate(freq_institution = n())%>%
-  ungroup()%>%
-  mutate(type = ifelse(is.na(type),"",type),
-         type = ifelse(type %in% c("livre","article","colloque","thèse"),"académique",type),
-         Désignation = designation_recoding(type,Auteur_Institution, Institution_Domaine, freq_auteur,freq_institution)
-  )
-
-recat <- read_excel("../data/intermediate/reseau/recategorize_designation.xlsx")%>%
-  select(-count)
-
-metadata_citation <- metadata_citation%>%
-  left_join(recat, by = "Désignation")%>%
-  rename(Precise_Désignation = Désignation,
-         Désignation = Rename)
-
-# Création des dataframes nécessaires
-reseau_citation_extra <- metadata_citation %>%
-  rename(Avis = cité_dans_avis_num) %>%
-  group_by(Avis, Désignation, Groupe) %>%
-  summarise(Citations = n()) %>%
-  rename(from = Avis,
-         to = Désignation,
-         Categorie = Groupe) %>%
-  mutate(shape = "Extra citation")
-
-reseau_citation_intra <- metadata_avis %>%
-  filter(!(is.na(citations_autres_avis))) %>%
-  separate_rows(citations_autres_avis, sep = ";") %>%
-  rename(from = num,
-         to = citations_autres_avis) %>%
-  group_by(from, to) %>%
-  summarise(Citations = n()) %>%
-  mutate(shape = "Intra citation",
-         Categorie = "CCNE",
-         from = as.character(from))
-
-mesograph_edgelist_df <- bind_rows(reseau_citation_extra, reseau_citation_intra) %>%
-  filter(!(to == "covid"))  # supprimer les avis covid pour l'instant
-
-mesograph_nodes_df <- bind_rows(
-  metadata_citation %>%
-    rename(name = Désignation, Categorie = Groupe) %>%
-    mutate(shape = "Extra citation") %>%
-    select(name, Categorie, shape) %>%
-    distinct(),
-  metadata_avis %>%
-    mutate(name = as.character(num),
-           shape = "Intra citation",
-           Categorie = "CCNE") %>%
-    select(name, Categorie, shape) %>%
-    distinct()
-) %>%
-  filter(!(is.na(name)))
-
-# Création de l'objet tbl_graph
-reseau_citation <- tbl_graph(edges = mesograph_edgelist_df %>% 
-                               select(from, to, Citations, Categorie, shape),
-                             nodes = mesograph_nodes_df %>%
-                               select(name, Categorie, shape),
-                             directed = TRUE, node_key = "name") %>%
-  activate(nodes) %>%
-  filter(degree(.) > 0)
-
-# Convertir l'objet tbl_graph en igraph
-reseau_citation_igraph <- as.igraph(reseau_citation)
-
 library(shiny)
 library(dplyr)
 library(tidyr)
 library(igraph)
 library(visNetwork)
+
+# Charger les objets pré-construits
+load("network_data.RData")
 
 # Couleurs fixes pour chaque catégorie
 category_colors <- c(
@@ -131,21 +19,37 @@ category_colors <- c(
   "Organismes recherches" = "#2C3324",
   "Presse" = "#022240",
   "Science, littérature" = "#21063F",
-  "Société" = "#3D0620"
+  "Société" = "#3D0620",
+  "Avis étudié" = "#022013"
 )
 
 # UI
 ui <- fluidPage(
-  titlePanel("Visualisation du Réseau de Citations"),
+  titlePanel("Visualisation de l'égo réseau des citations autour d'un avis"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("node_id", "Sélectionner le noeud (num):", 
+      selectInput("node_id", "Sélectionner le numéro de l'avis:", 
                   choices = sort(as.numeric(mesograph_nodes_df$name[mesograph_nodes_df$Categorie == "CCNE"])), 
                   selected = min(as.numeric(mesograph_nodes_df$name[mesograph_nodes_df$Categorie == "CCNE"]), na.rm = TRUE)),
       checkboxGroupInput("categories", "Choisir les catégories à afficher:",
                          choices = unique(mesograph_nodes_df$Categorie),
                          selected = unique(mesograph_nodes_df$Categorie)),
-      actionButton("toggle_select", "Décocher/Recocher toutes les catégories")
+      actionButton("toggle_select", "Décocher/Recocher toutes les catégories"),
+      actionButton("toggle_non_direct", "Enlever/Mettre les liens non directs"),
+      br(),
+      tags$div(
+        tags$h4("Légende des flèches"),
+        tags$div(
+          style = "display: flex; align-items: center;",
+          tags$div(style = "width: 20px; height: 2px; background-color: black; margin-right: 5px;"),
+          "Citations directes"
+        ),
+        tags$div(
+          style = "display: flex; align-items: center; margin-top: 5px;",
+          tags$div(style = "width: 20px; height: 2px; border-top: 2px dashed gray; margin-right: 5px;"),
+          "Citations indirectes"
+        )
+      )
     ),
     mainPanel(
       visNetworkOutput("network", height = "800px")
@@ -157,6 +61,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   all_categories <- unique(mesograph_nodes_df$Categorie)
   current_state <- reactiveVal(TRUE)  # Initial state is all categories selected
+  show_non_direct <- reactiveVal(TRUE)  # Initial state is to show non-direct links
   
   observeEvent(input$toggle_select, {
     if (current_state()) {
@@ -167,28 +72,57 @@ server <- function(input, output, session) {
     current_state(!current_state())  # Toggle the state
   })
   
+  observeEvent(input$toggle_non_direct, {
+    show_non_direct(!show_non_direct())  # Toggle the state for non-direct links
+  })
+  
   output$network <- renderVisNetwork({
     selected_node <- as.character(input$node_id)
     selected_categories <- input$categories
+    show_non_direct_val <- show_non_direct()
     
     # Filtrer les noeuds et les liens pour afficher seulement ceux à une profondeur de 1 du noeud sélectionné
     subgraph_nodes <- neighborhood(reseau_citation_igraph, order = 1, nodes = selected_node, mode = "all")[[1]]
     subgraph <- induced_subgraph(reseau_citation_igraph, subgraph_nodes)
     
-    # Filtrer par catégories sélectionnées
-    subgraph <- delete.vertices(subgraph, V(subgraph)[!(V(subgraph)$Categorie %in% selected_categories)])
+    # Ajouter "Avis étudié" à la liste des catégories sélectionnées
+    if ("CCNE" %in% selected_categories) {
+      selected_categories <- c(selected_categories, "Avis étudié")
+    }
+    
+    # Filtrer par catégories sélectionnées, en gardant "Avis étudié"
+    subgraph <- delete.vertices(subgraph, V(subgraph)[!(V(subgraph)$Categorie %in% selected_categories | V(subgraph)$name == selected_node)])
     
     # Convertir le sous-graph en format compatible avec visNetwork
     nodes <- data.frame(id = V(subgraph)$name, label = V(subgraph)$name, group = V(subgraph)$Categorie)
     edges <- as.data.frame(get.data.frame(subgraph, what = "edges"))
     
+    # Mettre à jour la catégorie et les attributs du noeud sélectionné
+    if (nrow(nodes) == 0) {
+      nodes <- data.frame(id = selected_node, label = selected_node, group = "Avis étudié", size = 30, color = "#022013")
+      edges <- data.frame(from = character(0), to = character(0), color = character(0), dashes = logical(0), width = numeric(0))
+    } else {
+      nodes <- nodes %>%
+        mutate(group = ifelse(id == selected_node, "Avis étudié", group),
+               size = ifelse(id == selected_node, 30, 15))  # Augmenter la taille du noeud sélectionné
+    }
+    
     # Appliquer les couleurs fixes aux catégories
     nodes$color <- category_colors[nodes$group]
     
-    # Mettre en gras les liens à partir du noeud sélectionné et les autres en pointillé
-    edges$color <- ifelse(edges$from == selected_node | edges$to == selected_node, "black", "gray")
-    edges$dashes <- ifelse(edges$from == selected_node | edges$to == selected_node, FALSE, TRUE)
-    edges$width <- ifelse(edges$from == selected_node | edges$to == selected_node, 2, 1)
+    # Mettre en gras les liens à partir du noeud sélectionné et les autres en pointillé ou les enlever
+    if (nrow(edges) > 0) {
+      if (show_non_direct_val) {
+        edges <- edges %>%
+          mutate(color = ifelse(from == selected_node | to == selected_node, "black", "gray"),
+                 dashes = ifelse(from == selected_node | to == selected_node, FALSE, TRUE),
+                 width = ifelse(from == selected_node | to == selected_node, 2, 1))
+      } else {
+        edges <- edges %>%
+          filter(from == selected_node | to == selected_node) %>%
+          mutate(color = "black", dashes = FALSE, width = 2)
+      }
+    }
     
     # Créer le graphique visNetwork
     visNetwork(nodes, edges) %>%
@@ -204,10 +138,11 @@ server <- function(input, output, session) {
       visGroups(groupname = "Presse", color = "#022240", shape = "triangle") %>%
       visGroups(groupname = "Science, littérature", color = "#21063F", shape = "triangle") %>%
       visGroups(groupname = "Société", color = "#3D0620", shape = "triangle") %>%
-      addFontAwesome()%>%
+      visGroups(groupname = "Avis étudié", color = "#022013", shape = "square") %>%
+      addFontAwesome() %>%
       visLegend(useGroups = TRUE, position = "right", width = 0.2, ncol = 2,
-                main = list(text = "Type de citations",
-                            style = "color:#000000;font-size:12px;text-align:center;")) %>%
+                main = list(text = "Catégorie de citations",
+                            style = "color:#000000;font-size:24px;text-align:center;")) %>%
       visEdges(arrows = 'to', color = list(color = "gray", highlight = "black")) %>%
       visNodes(font = list(size = 20))
   })
@@ -215,9 +150,3 @@ server <- function(input, output, session) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
-
-
-
-
-
