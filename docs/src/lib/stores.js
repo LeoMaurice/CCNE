1a4782a4c03bc733da43a0f645efb27b2a328a56
LeoMaurice <leopold.maurice@m4x.org>
2024-08-14 14:21:24 +0200

    redev as quarto website

--- docs/src/lib/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const selectedCategories = writable([]);
-export const avisRange = writable([125, 144]);
-export const networkVisible = writable(true);
